b04159002494fa8d782e0edba3748519c5d1beaf
ShashankPatil35 <shashankmpatil3535@gmail.com>
2024-04-24 12:14:15 +0530

    gemini-frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideBar from './components/Side-bar/SideBar'
+import Main from './components/Main/Main'
+function App() {
+  return (
+    <>
+      <SideBar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
